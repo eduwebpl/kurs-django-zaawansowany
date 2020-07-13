e26f946251872e9f4a9f621e9bd90cfa52b5c0c9
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-13 13:23:28 +0200

    05-finish

--- 02/app/core/tests/test_functions.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from core.functions import get_parsed_value_to_int_or_none
-
-
-class TestIntParser(TestCase):
-    def test_get_parsed_value_to_int_or_none(self):
-        result = get_parsed_value_to_int_or_none("1")
-        self.assertEqual(result, 1)
