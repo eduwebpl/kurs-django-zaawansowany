16015dc362c131ce73f0cfd0416856442102f835
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-14 18:55:55 +0200

    29-finish

--- 29/app/articles/views.py
+++ 29/app/articles/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import TrigramSimilarity
+from django.contrib.postgres.search import TrigramSimilarity, SearchRank, SearchVector, SearchQuery
 from django.views.generic import ListView, DetailView
 
 from articles.models import Article
@@ -14,8 +14,10 @@ class ArticleListView(ListView):
     def filter_queryset(self, queryset):
         query = self.request.GET.get('q')
         if query:
-            similarity = TrigramSimilarity('title', query)
-            queryset = queryset.annotate(similarity=similarity)
+            search_vector = SearchVector('title', weight="A") + SearchVector('content', weight='B')
+            search_query = SearchQuery(query)
+            rank = SearchRank(search_vector, search_query)
+            queryset = queryset.annotate(rank=rank).order_by('-rank').filter(rank__gt=0)
         return queryset
 
 
