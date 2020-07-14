d4e3274a1c1a39634263da94f8943365ea429bd7
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-14 18:15:54 +0200

    26-finish

--- 26/app/articles/views.py
+++ 26/app/articles/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from articles.models import Article
@@ -12,6 +13,12 @@ class ArticleListView(ListView):
 
     def filter_queryset(self, queryset):
         query = self.request.GET.get('q')
+        if query:
+            words = query.split(" ")
+            q_object = Q()
+            for word in words:
+                q_object.add(Q(title__icontains=word), Q.OR)
+            queryset = queryset.filter(q_object)
         return queryset
 
 
