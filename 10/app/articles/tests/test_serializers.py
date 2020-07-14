b99f808de5775e2f6919e555c72dc310a17d7285
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-14 11:10:57 +0200

    10-finish

---
+++ 10/app/articles/tests/test_serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+
+class SampleSerializer(serializers.Serializer):
+    sample_number = serializers.IntegerField(required=True)
+
+
+def test_validation():
+    valid_serializer = SampleSerializer(data={"sample_number": 1})
+    assert valid_serializer.is_valid()
+    invalid_serializer = SampleSerializer(data={"sample_number": "ab"})
+    assert not invalid_serializer.is_valid()
+    assert "sample_number" in invalid_serializer.errors
+    valid_serializer = SampleSerializer(data={"sample_number": "1"})
+    assert valid_serializer.is_valid()
+
+
+def test_data_field():
+    valid_serializer = SampleSerializer(data={"sample_number": "1"})
+    valid_serializer.is_valid()
+    assert valid_serializer.data["sample_number"] == 1
