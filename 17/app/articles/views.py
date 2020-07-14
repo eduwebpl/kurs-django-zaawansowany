8a8f42cd64556b8e08922d220d515f1c0be1cfad
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-14 14:38:15 +0200

    17-finish

--- 17/app/articles/views.py
+++ 17/app/articles/views.py
@@ -1,10 +1,11 @@
+from django.db.models import Prefetch
 from django.views.generic import ListView, DetailView
 
-from articles.models import Article
+from articles.models import Article, ArticleTag
 
 
 class ArticleListView(ListView):
-    queryset = Article.objects.all().select_related("author")
+    queryset = Article.objects.all().prefetch_related("author", "tags")
 
 
 class ArticleDetailView(DetailView):
