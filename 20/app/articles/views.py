8a38a6e66de2ea3a25f9a973b3a39fb84327c7fc
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-14 15:57:51 +0200

    20-finish

--- 20/app/articles/views.py
+++ 20/app/articles/views.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
 from django.views.generic import ListView, DetailView
 
 from articles.models import Article
@@ -5,6 +8,12 @@ from articles.models import Article
 
 class ArticleListView(ListView):
     queryset = Article.objects.all()
+
+    @method_decorator(cache_page(60))
+    @method_decorator(vary_on_headers('User-Agent'))
+    @method_decorator(vary_on_cookie)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class ArticleDetailView(DetailView):
