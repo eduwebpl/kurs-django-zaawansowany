60ac36d76084e1ca32828023b980fb3a605580bd
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-14 17:22:00 +0200

    23-finish

--- 24/app/products/models.py
+++ 24/app/products/models.py
@@ -7,3 +7,20 @@ class Product(models.Model):
 
     def __str__(self):
         return f"Produkt {self.title} ${self.price}"
+
+    class Meta:
+        abstract = True
+
+
+class Ebook(Product):
+    url_toc = models.URLField()
+
+    def __str__(self):
+        return f"Ebook {self.title} ${self.price}"
+
+
+class Book(Product):
+    isbn = models.TextField()
+
+    def __str__(self):
+        return f"Książka {self.title} ${self.price}"
