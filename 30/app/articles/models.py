c092044f117fe3b3aed7c26c3334705ff803985d
Grzegorz Szymborski <g.szymborski@p-programisci.pl>
2020-07-15 11:42:00 +0200

    30-finish

--- 30/app/articles/models.py
+++ 30/app/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F
 
 from core.mixins import CreateModifiedMixin
 
@@ -21,6 +22,11 @@ class Article(CreateModifiedMixin):
         related_name="articles",
         on_delete=models.CASCADE
     )
+    views = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
+
+    def increment_views(self):
+        self.views = F('views') + 1
+        self.save()
